40c25e42fa5a6d973a7a9e099b50c4ab43d668b4
Victor Bolum <victor.bolum@cyberspace.net.ng>
2021-01-02 10:25:45 +0100

    update

---
+++ DemoApp/DemoAppTest/IntegrationTest.cs
@@ -0,0 +1,49 @@
+﻿using DemoApp.Interfaces.Managers;
+using DemoApp.Interfaces.Services;
+using DemoApp.Managers;
+using DemoApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using DemoApp;
+
+namespace DemoAppTest
+{
+    [TestClass]
+    public class IntegrationTest
+    {
+
+        private HttpClient httpClient { get; set; }
+        public IntegrationTest()
+        {
+            var appfactory = new WebApplicationFactory<Startup>();
+            httpClient = appfactory.CreateClient();
+        }
+
+
+        [TestMethod]
+        public async Task IncrementCountTest()
+        {
+            var http = new HttpClient();
+            var rewe = await http.GetAsync("https://google.com");
+
+            var response = await httpClient.GetAsync("api/binlist/count");
+
+            response.EnsureSuccessStatusCode();
+
+            var res = await response.Content.ReadAsStringAsync();
+
+            return;
+        }
+
+
+    }
+}
--- RabbitMqConsumer/RabbitMqConsumer/Program.cs
+++ RabbitMqConsumer/RabbitMqConsumer/Program.cs
@@ -23,22 +23,9 @@ namespace RabbitMqConsumer
                 };
 
                 var connection = factory.CreateConnection();
-                var channel = connection.CreateModel();
 
-                channel.QueueDeclare("binlist", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine($"Message Received - {message}");
-                };
-
-                channel.BasicConsume("binlist", autoAck: true, consumer);
-
-                Console.WriteLine("Consumer up and running");
-
+                binlistListener(connection);
+                testListener(connection);
                 Console.Read();
             }
             catch (Exception ex)
@@ -47,6 +34,45 @@ namespace RabbitMqConsumer
                 Console.WriteLine(ex.Message);
                 Console.Read();
             }
+        }
+
+        public static void testListener(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("test", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Message Received on test queue - {message}");
+            };
+
+            channel.BasicConsume("test", autoAck: true, consumer);
+
+            Console.WriteLine("Listening for messages on test queue");
+        }
+
+
+
+        public static void binlistListener(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("binlist", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Message Received on bin queue - {message}");
+            };
+
+            channel.BasicConsume("binlist", autoAck: true, consumer);
+            Console.WriteLine("Listening for messages on binlist queue");
         }
 
     }
